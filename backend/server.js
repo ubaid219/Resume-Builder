9a3203adb984efbaa4eefeeaf617733f73b937e5
Ubaid Shafi <158993070+ubaid219@users.noreply.github.com>
2025-01-25 12:18:44 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,7 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: "https://resumo-frontend.onrender.com", credentials: true }));
 
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true, limit: "16kb" }));
