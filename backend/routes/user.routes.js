18314c0cde3edbf8753d35ed1a5a6bc38df88fe1
Ubaid Shafi <158993070+ubaid219@users.noreply.github.com>
2025-01-25 12:32:42 +0530

    Update user.routes.js

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -7,6 +7,6 @@ const userRouter = Router();
 userRouter.route("/register").post(registerUser);
 userRouter.route("/login").post(loginUser);
 userRouter.route("/logout").get(verifyJWT, logoutUser);
-userRouter.route("/getuser").get( getUser);
+userRouter.route("/getUser").get( getUser);
 
 export default userRouter;
